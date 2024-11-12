daf370297b6b046084b339d0aaad6b8a3232da1b
tung <tungnt9905@gmail.com>
2024-11-12 17:14:42 +0700

    k8snodeF

--- nodejs-demo-k8s/cleanimage.sh
+++ nodejs-demo-k8s/cleanimage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 APP_NAME="my-nodejs-app"
 KEEP_IMAGES=2
-docker images --filter=reference="$APP_NAME:*" --format '{{.ID}} {{.Repository}}:{{.Tag}} {{.CreatedAt}}' |
-  sort -r -k3 |
-  tail -n +$((KEEP_IMAGES + 1)) |
-  awk '{print $1}' 
+docker images --filter=reference="$APP_NAME:*" --format '{{.CreatedAt}} {{.ID}}' | \
+  sort -k2 | 
+  head -n -$KEEP_IMAGES | 
+  awk '{print $5}' | 
   xargs -r docker rmi -f
+
 sudo crictl images | grep "$APP_NAME" |
-  sort -r -k4 |
-  tail -n +$((KEEP_IMAGES + 1)) |
-  awk '{print $3}' 
+  sort -k2 |
+  head -n -$KEEP_IMAGES | 
+  awk '{print $3}' |
   xargs -r sudo crictl rmi
